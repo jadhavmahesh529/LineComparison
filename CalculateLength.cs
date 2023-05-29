2619c10f7a416e8a07c8d0ce5b8d8ae2eef4313a
“Enter <jadhavmahesh529@gmail.com>
2023-05-29 22:49:31 +0530

    Add: calculate length code is added

---
+++ CalculateLength.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparison
+{
+    internal class CalculateLength
+    {
+        public void calculatelength()
+        {
+            Console.WriteLine("Enter the coordinates of Point x1:");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the coordinates of Point y1:");
+            double y1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the coordinates of Point x2:");
+            double x2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the coordinates of Point y2:");
+            double y2 = Convert.ToDouble(Console.ReadLine());
+
+
+            double length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+
+            Console.WriteLine($"The length of the line is: {length}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace LineComparison
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to Line Comparison Computation Program");
+            CalculateLength calculator = new CalculateLength();
+            calculator.calculatelength();
+        }
+    }
+}
