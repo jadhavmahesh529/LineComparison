cedf3c69015ef5494b7ecadb2bc91e405919c3cc
“Enter <jadhavmahesh529@gmail.com>
2023-05-29 23:24:14 +0530

    Add: Check Oops code is added

---
+++ Oops.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparison
+{
+    internal class Oops
+    {
+        public interface ILengthCalculate
+        {
+            double CalculateLength();
+        }
+
+        public class Point
+        {
+            public double X { get; set; }
+            public double Y { get; set; }
+
+            public Point(double x, double y)
+            {
+                X = x;
+                Y = y;
+            }
+        }
+
+        public class Line : ILengthCalculate
+        {
+            public Point Point1 { get; set; }
+            public Point Point2 { get; set; }
+
+            public Line(Point point1, Point point2)
+            {
+                Point1 = point1;
+                Point2 = point2;
+            }
+
+            public double CalculateLength()
+            {
+                double length = Math.Sqrt(Math.Pow(Point2.X - Point1.X, 2) + Math.Pow(Point2.Y - Point1.Y, 2));
+                return length;
+            }
+            public void CheckEquality(Line Line2)
+            {
+                bool areEqual = Point1.X == Line2.Point1.X && Point1.Y == Line2.Point1.Y && Point2.X == Line2.Point2.X && Point2.Y == Line2.Point2.Y;
+
+                Console.WriteLine("Lines are" + (areEqual ? " " : " not ") + "equal.");
+            }
+            public void CompareLines(Line Line2)
+            {
+                double length1 = CalculateLength();
+                double length2 = Line2.CalculateLength();
+
+                int comparison = length1.CompareTo(length2);
+
+                if (comparison == 0)
+                {
+                    Console.WriteLine("Both lines are equal in length.");
+                }
+                else if (comparison > 0)
+                {
+                    Console.WriteLine("Line 1 is greater in length than Line 2.");
+                }
+                else
+                {
+                    Console.WriteLine("Line 1 is less in length than Line 2.");
+                }
+            }
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace LineComparison
+﻿using static LineComparison.Oops;
+
+namespace LineComparison
 {
     internal class Program
     {
@@ -15,6 +17,35 @@
             Comparetwolines comparetwolines = new Comparetwolines();
             comparetwolines.comparetwolines();
 
+            Console.WriteLine("Enter the coordinates of the endpoints for Line 1:");
+            double point1Line1 = Convert.ToDouble(Console.ReadLine());
+            double point2Line1 = Convert.ToDouble(Console.ReadLine());
+            double point3Line1 = Convert.ToDouble(Console.ReadLine());
+            double point4Line1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the coordinates of the endpoints for Line 2:");
+            double point1Line2 = Convert.ToDouble(Console.ReadLine());
+            double point2Line2 = Convert.ToDouble(Console.ReadLine());
+            double point3Line2 = Convert.ToDouble(Console.ReadLine());
+            double point4Line2 = Convert.ToDouble(Console.ReadLine());
+
+            Point point1 = new Point(point1Line1, point2Line1);
+            Point point2 = new Point(point3Line1, point4Line1);
+            Point point3 = new Point(point1Line2, point2Line2);
+            Point point4 = new Point(point3Line2, point4Line2);
+
+            Line line1 = new Line(point1, point2);
+            Line line2 = new Line(point3, point4);
+
+            double length1 = line1.CalculateLength();
+            double length2 = line2.CalculateLength();
+
+            Console.WriteLine("Line 1 Length: " + length1);
+            Console.WriteLine("Line 2 Length: " + length2);
+
+            line1.CheckEquality(line2);
+
+            line1.CompareLines(line2);
 
         }
     }
